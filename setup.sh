52c08d84ef46b911ae5b77c547d32b0d6c21f058
Weiqiang Zhu <75299929+zhuwq0@users.noreply.github.com>
2025-04-07 02:24:26 +0000

    update

--- setup.sh
+++ setup.sh
@@ -3,16 +3,11 @@ set -x
 sudo apt update
 sudo apt install gfortran -y
 # python3 -m pip install -r requirements.txt
-# conda env update -f=env.yaml -n base
-# conda create --yes --prefix .conda/quakeflow
-# conda env update -f=env.yaml --prefix .conda/quakeflow
 conda env create -f env.yaml --prefix .conda/quakeflow
-source activate .conda/quakeflow
-conda install --y cartopy s3fs obspy nb_conda_kernels ipykernel jupyter_server
-conda env export --prefix .conda/quakeflow > env.yaml
+# source activate .conda/quakeflow
+# conda env export --prefix .conda/quakeflow > env.yaml
 
-
-# git clone --recursive https://github.com/AI4EPS/QuakeFlow.git notebooks/QuakeFlow/
-# cd notebooks/QuakeFlow/PhaseNet && git checkout master && cd ../
-# python -m pip install -U git+https://github.com/AI4EPS/GaMMA.git
-# python -m pip install -U git+https://github.com/AI4EPS/ADLoc.git
+git clone --recursive https://github.com/AI4EPS/QuakeFlow.git notebooks/QuakeFlow/
+cd notebooks/QuakeFlow/PhaseNet && git checkout master && cd ../
+python -m pip install -U git+https://github.com/AI4EPS/GaMMA.git
+python -m pip install -U git+https://github.com/AI4EPS/ADLoc.git
