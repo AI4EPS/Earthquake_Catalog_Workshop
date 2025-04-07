b68de36f77542c31b31080f93a8a9c1ccd3cd4c8
Weiqiang Zhu <75299929+zhuwq0@users.noreply.github.com>
2025-04-07 01:03:35 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -x
 sudo apt update
-sudo apt install gfortran libxpm4 csh -y
+sudo apt install gfortran -y
 # python3 -m pip install -r requirements.txt
 # conda env update -f=env.yaml -n base
-conda create --yes --prefix .conda --name quakeflow
-conda env update -f=env.yaml -n quakeflow
+# conda create --yes --prefix .conda/quakeflow
+# conda env update -f=env.yaml --prefix .conda/quakeflow
+conda env create -f env.yaml --prefix .conda/quakeflow
+
 
 git clone --recursive https://github.com/AI4EPS/QuakeFlow.git notebooks/QuakeFlow/
 cd notebooks/QuakeFlow/PhaseNet && git checkout master && cd ../
