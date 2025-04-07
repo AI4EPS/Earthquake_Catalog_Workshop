d8171066cecec520e90e4b4bd3e6f2b0f1969a92
Weiqiang Zhu <75299929+zhuwq0@users.noreply.github.com>
2025-04-07 01:48:35 +0000

    update

--- setup.sh
+++ setup.sh
@@ -8,8 +8,9 @@ sudo apt install gfortran -y
 # conda env update -f=env.yaml --prefix .conda/quakeflow
 conda env create -f env.yaml --prefix .conda/quakeflow
 source activate .conda/quakeflow
+conda install cartopy s3fs obspy
 
 # git clone --recursive https://github.com/AI4EPS/QuakeFlow.git notebooks/QuakeFlow/
 # cd notebooks/QuakeFlow/PhaseNet && git checkout master && cd ../
-# python -m pip install -q -U git+https://github.com/AI4EPS/GaMMA.git
-# python -m pip install -q -U git+https://github.com/AI4EPS/ADLoc.git
+# python -m pip install -U git+https://github.com/AI4EPS/GaMMA.git
+# python -m pip install -U git+https://github.com/AI4EPS/ADLoc.git
