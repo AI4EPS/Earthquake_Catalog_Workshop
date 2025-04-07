3cea856e7119bce65b5ecefe30968cbfd8612c0a
Weiqiang Zhu <75299929+zhuwq0@users.noreply.github.com>
2025-04-07 01:23:56 +0000

    update

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ sudo apt install gfortran -y
 # conda create --yes --prefix .conda/quakeflow
 # conda env update -f=env.yaml --prefix .conda/quakeflow
 conda env create -f env.yaml --prefix .conda/quakeflow
+source activate .conda/quakeflow
 
-
-git clone --recursive https://github.com/AI4EPS/QuakeFlow.git notebooks/QuakeFlow/
-cd notebooks/QuakeFlow/PhaseNet && git checkout master && cd ../
+# git clone --recursive https://github.com/AI4EPS/QuakeFlow.git notebooks/QuakeFlow/
+# cd notebooks/QuakeFlow/PhaseNet && git checkout master && cd ../
+# python -m pip install -q -U git+https://github.com/AI4EPS/GaMMA.git
+# python -m pip install -q -U git+https://github.com/AI4EPS/ADLoc.git
